70dadc14030b958c1fcc95e222d99c8327dbcf11
Matthew Kerns <matthew.kerns@MKerns-Macbook-Pro.local>
2020-04-16 17:53:39 -0400

    Broken homepage into smaller components / finshed styling menu-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './screens/hompage/homepage.component';
 
 function App() {
 	return (
