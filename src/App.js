8f2ef3a1934aff4bb3b8d97e0c71b47c1116448d
Matthew Kerns <matthew.kerns@MKerns-Macbook-Pro.local>
2020-04-19 22:37:19 -0400

    Implemented firebase utils / added ability to store authenticated users into firestore database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './screens/hompage/homepage.component';
 import ShopPage from './screens/shop/shop.component';
 import SignInAndSignUp from './screens/sign-in-and-sign-up/sign-in-and-sign-up.components';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 	constructor() {
@@ -21,10 +21,21 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({ currentUser: user });
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
 
-			console.log(user);
+				userRef.onSnapshot(snapShot => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data()
+						}
+					});
+				});
+			} else {
+				this.setState({ currentUser: userAuth });
+			}
 		});
 	}
 
--- src/screens/sign-in-and-sign-up/sign-in-and-sign-up.components.jsx
+++ src/screens/sign-in-and-sign-up/sign-in-and-sign-up.components.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.styles.scss';
 
@@ -8,6 +9,7 @@ const SignInAndSignUp = () => {
 	return (
 		<div className='sign-in-and-sign-up'>
 			<SignIn />
+			<SignUp />
 		</div>
 	);
 };
